889c3ac2383ea81c166d4e3f1b807a6ace28c43f
faster-tafi <aminetafi588@gmail.com>
2025-08-31 00:59:55 +0100

    تحديث البيانات من لوحة التحكم

--- level.js
+++ level.js
@@ -73,13 +73,7 @@ const levelSubjects = {
         {
             "name": "الاجتماعيات",
             "icon": "🌍",
-            "lessons": [
-                {
-                    "title": "التنمية الاقتصادية",
-                    "downloadNumber": "03",
-                    "downloadUrl": "https://drive.google.com/file/d/YOUR_FILE_ID_HERE/view?usp=sharing"
-                }
-            ],
+            "lessons": [],
             "exercises": [
                 {
                     "title": "دراسة وثائق تاريخية",
