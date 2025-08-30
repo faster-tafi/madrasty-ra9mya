679c5b17ea4895e3866313060d6b55f762b87f6e
faster-tafi <aminetafi588@gmail.com>
2025-08-31 00:55:45 +0100

    تحديث البيانات من لوحة التحكم

--- level.js
+++ level.js
@@ -62,13 +62,7 @@ const levelSubjects = {
         {
             "name": "الرياضيات",
             "icon": "📐",
-            "lessons": [
-                {
-                    "title": "الإحصاء",
-                    "downloadNumber": "03",
-                    "downloadUrl": "https://drive.google.com/file/d/YOUR_FILE_ID_HERE/view?usp=sharing"
-                }
-            ],
+            "lessons": [],
             "exercises": []
         },
         {
