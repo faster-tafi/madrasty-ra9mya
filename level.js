6baa6c71bdf2e8871c8f83fbb257ddf7b8c6a762
faster-tafi <aminetafi588@gmail.com>
2025-08-31 01:45:39 +0100

    تحديث البيانات من لوحة التحكم

--- level.js
+++ level.js
@@ -105,11 +105,6 @@ const levelSubjects = {
             "icon": "🤔",
             "lessons": [
                 {
-                    "title": "مدخل إلى الفلسفة",
-                    "downloadNumber": "01",
-                    "downloadUrl": "https://drive.google.com/file/d/YOUR_FILE_ID_HERE/view?usp=sharing"
-                },
-                {
                     "title": "فلسفة المعرفة",
                     "downloadNumber": "03",
                     "downloadUrl": "https://drive.google.com/file/d/YOUR_FILE_ID_HERE/view?usp=sharing"
