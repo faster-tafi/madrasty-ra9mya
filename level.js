c6abc43318aa290f8d9de81f2cb726aef6704930
faster-tafi <aminetafi588@gmail.com>
2025-08-31 20:25:44 +0100

    تحديث البيانات من لوحة التحكم

--- level.js
+++ level.js
@@ -78,11 +78,6 @@ const levelSubjects = {
             ],
             "exercises": [
                 {
-                    "title": "تمارين البرمجة",
-                    "downloadNumber": "01",
-                    "downloadUrl": "https://drive.google.com/file/d/YOUR_FILE_ID_HERE/view?usp=sharing"
-                },
-                {
                     "title": "مشاريع تطبيقية",
                     "downloadNumber": "02",
                     "downloadUrl": "https://drive.google.com/file/d/YOUR_FILE_ID_HERE/view?usp=sharing"
