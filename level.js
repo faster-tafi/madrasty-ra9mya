1223dc8015c23b29a735d63b46e2726f6a5793aa
faster-tafi <aminetafi588@gmail.com>
2025-08-31 20:29:20 +0100

    تحديث البيانات من لوحة التحكم

--- level.js
+++ level.js
@@ -73,11 +73,6 @@ const levelSubjects = {
                     "title": "تحليل النصوص",
                     "downloadNumber": "01",
                     "downloadUrl": "https://drive.google.com/file/d/YOUR_FILE_ID_HERE/view?usp=sharing"
-                },
-                {
-                    "title": "تمارين البلاغة",
-                    "downloadNumber": "02",
-                    "downloadUrl": "https://drive.google.com/file/d/YOUR_FILE_ID_HERE/view?usp=sharing"
                 }
             ]
         },
