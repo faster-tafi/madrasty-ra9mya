df8a70db72b805a154546b5c65a12a1c87b436b6
faster-tafi <aminetafi588@gmail.com>
2025-08-31 01:49:04 +0100

    تحديث البيانات من لوحة التحكم

--- level.js
+++ level.js
@@ -153,11 +153,6 @@ const levelSubjects = {
             ],
             "exercises": [
                 {
-                    "title": "مسائل الوراثة المعقدة",
-                    "downloadNumber": "01",
-                    "downloadUrl": "https://drive.google.com/file/d/YOUR_FILE_ID_HERE/view?usp=sharing"
-                },
-                {
                     "title": "تجارب المناعة",
                     "downloadNumber": "02",
                     "downloadUrl": "https://drive.google.com/file/d/YOUR_FILE_ID_HERE/view?usp=sharing"
