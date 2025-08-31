c5a189cf16c86966def2060b2887d0e3e3696190
faster-tafi <aminetafi588@gmail.com>
2025-08-31 01:45:13 +0100

    تحديث البيانات من لوحة التحكم

--- level.js
+++ level.js
@@ -100,11 +100,6 @@ const levelSubjects = {
             "lessons": [],
             "exercises": [
                 {
-                    "title": "مسائل جبرية معقدة",
-                    "downloadNumber": "01",
-                    "downloadUrl": "https://drive.google.com/file/d/YOUR_FILE_ID_HERE/view?usp=sharing"
-                },
-                {
                     "title": "تمارين هندسية",
                     "downloadNumber": "02",
                     "downloadUrl": "https://drive.google.com/file/d/YOUR_FILE_ID_HERE/view?usp=sharing"
